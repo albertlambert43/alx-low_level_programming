4cedd2c04d110097460f5bee0b0c297276a6df63
albertlambert43 <albertlambert43@gmail.com>
2023-03-17 15:34:35 -0700

    putchar twice usage

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z');
+	{
+		if ((c != 'q' && c != 'e') && c <= 'z')
+		putchar(c);
+		c++;
+	}
+	printf('\n');
+	return (0);
+}
