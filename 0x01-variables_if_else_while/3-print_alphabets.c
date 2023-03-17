2b917d156badc124dffcc2271abaf5a055ea6706
albertlambert43 <albertlambert43@gmail.com>
2023-03-17 15:05:51 -0700

    lower case then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	char c;
+
+	 c = 'a';
+	 d = 'A';
+	 while
+		 (c <= 'z') {
+			 putchar(c);
+			 c++;
+		 }
+	 while
+		 d <= 'Z') {
+			 putchar (d);
+			 d++;
+		 }
+	 putchar('\n');
+	 return (0);
+}
+
+
+
